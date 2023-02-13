6fab552bde36bdc55a503723f542d9a0b51780f1
rashidkhan <rashidkhanrk5456@email.com>
2023-02-13 19:49:53 +0530

    js-text-3

---
+++ script.js
@@ -0,0 +1,164 @@
+
+
+
+
+                        //  exercise-3
+
+                        // start question and answer
+
+// ans-1
+// Create a variable marks and assign any number. If marks are greater than 60 then print Pass else print Fail.
+
+// let marks = 59;
+// if (marks >= 60) {
+//     console.log("Pass");
+// } else {
+//     console.log("Fail");
+// }
+
+// ans-2
+
+// Take two numbers and check if both numbers are odd or not. If yes then print Odd otherwise print Even
+
+// let num = 35;
+// let num2 = 34;
+// if (num % 2 === 1 && num2 % 2 === 1) {
+//     console.log("Odd");
+// } else {
+//     console.log("Even");
+// }
+
+// ans-3
+
+// Take 4 variables a, b, c, d and assign their values in the way mentioned below and check output: a = b b = c c = d d = a
+
+// let a=12;
+// let b=2;
+// let c=3;
+// let d=8;
+
+// // a=2 ;
+// // b= 3 ;
+// // c=8;
+// // d=2
+
+// console.log(a=b);
+// console.log(b=c);
+// console.log(c=d);
+// console.log(d=a);
+
+// ans-4
+
+// Print 2's table using ** operator. Use any logic but it should print 2's table.
+
+// ** operator
+
+// console.log(2**1);
+// console.log(2**2);
+// console.log(2**3-2);
+// console.log(2**3);
+// console.log(2**4-6);
+// console.log(2**4-4);
+// console.log(2**4-2);
+// console.log(2**4);
+// console.log(2**4+2);
+// console.log(2**4+4);
+
+// * operator
+
+// console.log(2*1);
+// console.log(2*2);
+// console.log(2*3);
+// console.log(2*4);
+// console.log(2*5);
+// console.log(2*6);
+// console.log(2*7);
+// console.log(2*8);
+// console.log(2*9);
+// console.log(2*10);
+
+// ans-5
+
+// Check output of following:
+
+// console.log(10 < 20 && 10 + 5 <= 15);
+// console.log((30 > 20 && 10 + 4 < 15) || 10);
+// console.log(30 < 20 && 10 + 5 <= 15 || 10);
+// console.log(30 < 20 || (10 + 5 < 15 && 10));
+
+// ans-6
+// Take a number and check if number is odd or not. If number is Odd then print Yes else print No
+
+// let a= 49;
+// if(a%2===1){
+//     console.log("yes")
+
+// }
+// else{
+// console.log("no")
+// }
+
+// ans-7
+// Take a number and check if number is divisible by 10 && 5. If yes then print Yes else print no
+
+// let a = 35;
+// if (a % 5 === 0 && a % 10 === 0) {
+//   console.log("yes");
+// } else {
+//   console.log("no");
+// }
+
+
+
+
+// ans-8
+
+// Take a number and check if number * 20 makes it 100 or not. If yes the print Yes else print No
+
+// let a=5;
+
+// if(a*20===100){
+//     console.log("yes")
+
+// }
+// else{
+//     console.log("no")
+// }
+
+
+
+// ans-9
+// Calculate your 10th percentage and print Pass or Fail if marks are more than 70 then print Pass else print Fail
+
+
+// let marks = 550;
+// let totalMarks = 1000;
+// let percentage = (marks / totalMarks) * 100;
+// if (percentage > 70) {
+//     console.log("Pass");
+// } else {
+//     console.log("Fail");
+// }
+
+
+
+
+// ans-10
+// Take your age and check if your age is greater than 21 then print Adult else print Minor
+
+// let bornyear=2000;
+// let cunrretyear=2023;
+// let age=cunrretyear-bornyear;
+
+
+// if(age>21){
+//     console.log("adult")
+// }
+// else{
+//     console.log("minor")
+// }
+
+
+
+//   end
+
