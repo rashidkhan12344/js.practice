766d6d79b55815c1143a63bd07518e8b3c54df9b
rashidkhan <rashidkhanrk5456@email.com>
2023-02-15 22:43:19 +0530

    add

---
+++ test-3 js/mytest.js
@@ -0,0 +1,167 @@
+
+
+
+       // test
+
+
+       
+// ans-1
+
+// let a = Number(prompt("How many french fries"))
+// let b = Number(prompt(" How many burgers "));
+// let c = Number(prompt(" How many plates of chowmin "));
+// let d = Number(prompt("How many plates of Manchurian "))
+// let e = Number(prompt("How many Cokes "))
+
+// let frenchfries = a * 60
+// let burgers = b *50 
+// let chowmin = c *100 
+// let Manchurian = d *80 
+// let Cokes = e * 50 
+
+// let total = frenchfries + burgers + chowmin + Manchurian + Cokes ;
+
+
+// console.log( `frenchfries : ${a} ; ${frenchfries}
+// burgers : ${b} ; ${burgers}
+// chowmin :${c} ; ${chowmin}
+// Manchurian :${d} ; ${Manchurian}
+// Cokes : ${e} ; ${Cokes}
+// total : ${total}
+// `  )
+
+
+
+
+// ans-2
+
+
+// for(let f =5;f>=1;f--){
+//        let ans="";
+//        for(let g = 1;g<=f;g++){
+//            ans=ans+"$";
+//        }
+//        console.log(ans)
+//        }
+
+// // ans-3
+
+
+// let num=prompt("How many times do you want to print Series");
+
+// for(let num=10;num<=100;num=num+10)
+
+// console.log(num);
+
+
+
+
+// // ans-4
+
+// let num = 103;
+// let ques = num;
+// switch (num) {
+ 
+//   case ques:
+//     if (num % 2 == 0) {
+//       console.log("even");
+//     } else if (num % 2 === 1) {
+//       console.log("odd");
+//     }
+//     break;
+
+//   default:
+//     console.log(10000);
+//     break;
+// }
+
+
+
+// ans-5
+
+// let birthyear=prompt("What is your birth year");
+// let age =21;
+
+// if(age>18){
+//     console.log (prompt("Do you have valid license number"));
+//     alert(age)
+   
+// }
+// else{
+// console.log (prompt("You are ready to drive with your #### (show gaurdian name here."));
+// alert("Sorry visit again please.");
+// }
+
+
+
+
+// ans-6
+
+
+// let num1 = 40;
+// let num2 = 19;
+// let num3 = num1 - num2;
+// if (num3 > 19) {
+//   console.log(num3 * 3);
+// } else {
+//   console.log(num3 * 2);
+// }
+
+
+
+
+
+// // ans-7
+
+// let one=20;
+// let two=30;
+
+// if(one%2===0 &&  two%2===0)
+// {
+//     console.log(one+two)
+// }
+// else{
+//     console.log(one*two);
+// }
+
+
+
+
+
+
+
+
+// // ans-8
+
+// let a= 10;
+// let b=32;
+// let c=20;
+// let d= 31;
+
+// if(a===b && b===c && c===d && d===a)
+// console.log("yes")
+
+// else{
+//     console.log("no");
+// }
+
+
+
+
+// // ans-10
+
+// let string = "wecodeacademy";
+// if (string.length > 10) {
+//   console.log(string.length);
+// } else {
+//   console.log("no");
+// }
+
+
+
+   
+
+
+
+
+
